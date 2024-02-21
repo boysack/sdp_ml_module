9dd83690096891d2efd2e742db80d0f0e0ad4ae8
boysack <claudiomacaluso@gmail.com>
2024-02-21 19:08:43 +0100

    setup train for 3 datasets

--- CVAE/train.py
+++ CVAE/train.py
@@ -11,7 +11,7 @@ def parse_arguments():
     parser.add_argument('--train', action='store_true', help='Begin training')
     parser.add_argument('--num_epochs', type=int, default=25, help='Epochs number')
     parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
-    parser.add_argument('--dataset_path', default="data/linear_interpolated_all.csv", type=str, help='Dataset path', metavar="")
+    parser.add_argument('--dataset_path', default="data/linear_interpolated", type=str, help='Dataset path', metavar="")
     parser.add_argument('--checkpoint_path', type=str, default=f"checkpoints/checkpoint-{int(time.time())}", help='Checkpoint path', metavar="")
     parser.add_argument('--gen_data',  action='store_true', help='Begin generating synthetic data')
     return parser
@@ -19,10 +19,21 @@ def parse_arguments():
 def train(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    num_epochs = 5
+
     model = CVAE(seq_len=1, feat_dim=15, conditional_dim=2, enc_out_dim=5, latent_dim=3, beta=1.0, learning_rate=0.005, min_std=0.025, checkpoint_path=args.checkpoint_path).to(device)
-    dataset = ArolDataset(args.dataset_path)
+    trainer = pl.Trainer(max_epochs=num_epochs)
+
+    dataset = ArolDataset(args.dataset_path+"_part01.csv")
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
-    trainer = pl.Trainer(max_epochs=args.num_epochs)
+    trainer.fit(model, dataloader)
+
+    dataset = ArolDataset(args.dataset_path+"_part02.csv")
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
+    trainer.fit(model, dataloader)
+
+    dataset = ArolDataset(args.dataset_path+"_part03.csv")
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=False)
     trainer.fit(model, dataloader)
     
 """ optimizer = model.configure_optimizers()
